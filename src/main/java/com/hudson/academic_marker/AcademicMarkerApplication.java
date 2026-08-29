008ab132ac5ca3a0feb44025e1c2a32ea75e25b3
Hudsk9 <18.ft14@gmail.com>
2026-08-29 01:26:31 -0300

    commit inicial: estrutura gerada no initialzr.

---
+++ src/main/java/com/hudson/academic_marker/AcademicMarkerApplication.java
@@ -0,0 +1,13 @@
+package com.hudson.academic_marker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademicMarkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcademicMarkerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hudson/academic_marker/AcademicMarkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hudson.academic_marker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademicMarkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
